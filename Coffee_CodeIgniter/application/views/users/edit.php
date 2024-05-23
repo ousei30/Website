8fb763366d4379d7964d07ff2ab7e1226c856484
Van Lester <56195979+ousei30@users.noreply.github.com>
2024-05-23 13:29:59 +0800

    Update edit.php

--- Coffee_CodeIgniter/application/views/users/edit.php
+++ Coffee_CodeIgniter/application/views/users/edit.php
@@ -1,30 +1,50 @@
-
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <div class="container-fluid">
-  <div class="container shadow mt-5 bg-warning text-white text-center">
-    <?php if(validation_errors()) :?>
-    <p><?= validation_errors();?></p>
-    <?php endif;?>
-  </div>
-  <div class="container p-5 shadow mt-4 bg-dark  border border-white">
+    <div class="container shadow mt-5 bg-warning text-white text-center">
+        <?php if(validation_errors()) :?>
+        <p><?= validation_errors();?></p>
+        <?php endif;?>
+    </div>
+    <div class="container p-5 shadow mt-4 bg-dark  border border-white"
+        style="background-image: url(<?= base_url('assets/image/try.jpg')?>); font-size: 15px;">
 
-      <label for="">Product Image : <span class="text-warning"></span></label>
-      <input class="form-control-file" type="file" name="img" id="">
 
-      <form method="post" action="<?= base_url('users/update')?>">
-      <label for="">username : <span class="text-warning"></span></label>
-      <input class="form-control" type="text" name="username" value= <?= $user->username; ?> id="">
+        <form method="post" action="<?= base_url('users/update')?>">
+            <label for="">Admin Image : <span class="text-warning"></span></label>
+            <input class="form-control-file" type="file" name="img" id="">
 
-      <label for="">firstname : <span class="text-warning"></span></label>
-      <input class="form-control" type="text" name="firstName" value= <?= $user->username; ?> id="">
+            <label for="">username : <span class="text-warning"></span></label>
+            <input class="form-control" type="text" name="username" value="<?= $user->username; ?>" id="">
 
-      <label for="">middlename : <span class="text-warning"></span></label>
-      <input class="form-control" type="text" name="middleName" value= <?= $user->username; ?> id="">
+            <div class="row">
+                <div class="col-4">
+                    <label for="">firstname : <span class="text-warning"></span></label>
+                    <input class="form-control" type="text" name="firstName" value="<?= $user->firstName; ?>" id="">
 
-      <label for="">lastname : <span class="text-warning"></span></label>
-      <input class="form-control" type="text" name="lastName" value= <?= $user->username; ?> id="">
+                </div>
+                <div class="col-4">
 
-      <input type="hidden" name="id"  value= <?= $user->id; ?>>
-      <input class="btn btn-success mt-2" type="submit" value="Update">
-      </form>
-  </div>
+                    <label for="">middlename : <span class="text-warning"></span></label>
+                    <input class="form-control" type="text" name="middleName" value=<?= $user->middleName; ?> id="">
+
+                </div>
+                <div class="col-4">
+                    <label for="">lastname : <span class="text-warning"></span></label>
+                    <input class="form-control" type="text" name="lastName" value=<?= $user->lastName; ?> id="">
+                </div>
+
+            </div>
+
+            <input type="hidden" name="id" value=<?= $user->id; ?>>
+            <input class="btn btn-success mt-2" type="submit" value="Update">
+        </form>
+    </div>
 </div>
+<br>
+<br>
+<br>
